8f434485cd58ec760f6c7806790564ed9251dbcc
dachieng <oloodorcas99@gmail.com>
2021-03-05 08:36:11 +0300

    loading content dynamically

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length = 100)
+    image:models.ImageField(upload_to = 'pics')
+    age: models.IntegerField()
+    description:models.TextField(max_length=200)
+    offer:models.BooleanField(default=False)
--- app/views.py
+++ app/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Team
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html", {"name":"Dorcas"})
+    team1 = Team()
+    team1.name = "Dorcas"
+    team1.age = 17
+    team1.description = " Im Dorcas and Im 17 years Old"
+    team1.offer = False
+    team1.image = "team1.jpg"
 
-def add(request):
-    num1 = int(request.GET["num1"])
-    num2 = int(request.GET["num2"])
-    result = num1 + num2 
+    team2 = Team()
+    team2.name = "John Galekki"
+    team2.age = 25
+    team2.description = " Im John and Im 20 years Old"
+    team2.offer = True
+    team2.image = "team2.jpg"
+    
+    teams = [team1, team2]
+    return render(request, "index.html", {"teams":teams})
 
-
-    return render(request, "add.html", {"result", result})
